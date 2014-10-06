8ea3dec2795ca608baadb961c3e65de867fb3e45
crysxd <c.wuerthner@me.com>
2014-10-06 18:21:24 +0200

    Added functionality to deserialize a json representation

--- src/de/crysxd/ownfx/ColorStop.java
+++ src/de/crysxd/ownfx/ColorStop.java
@@ -1,23 +1,52 @@
 package de.crysxd.ownfx;
 
+import java.awt.Color;
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
 public class ColorStop {
-	
-	private int color;
+
+	private Color color;
 	private int ledIndex;
-	
-	public int getColor() {
+
+	public Color getColor() {
 		return color;
 	}
-	
-	public void setColor(int color) {
+
+	public void setColor(Color color) {
 		this.color = color;
 	}
-	
+
 	public int getLedIndex() {
 		return ledIndex;
 	}
-	
+
 	public void setLedIndex(int ledIndex) {
 		this.ledIndex = ledIndex;
+	}
+
+
+	public static class JsonDeserializer implements com.google.gson.JsonDeserializer<ColorStop> {
+		
+		@Override
+		public ColorStop deserialize(JsonElement element, Type type,
+				JsonDeserializationContext context) throws JsonParseException {
+
+			ColorStop c = new ColorStop();
+
+			JsonObject json = element.getAsJsonObject();
+
+			JsonElement colorElement = json.get("color");
+			JsonElement ledIndexElement = json.get("ledIndex");
+
+			c.setLedIndex(ledIndexElement.getAsInt());
+			c.setColor(Color.decode(colorElement.getAsString()));
+
+			return c;
+		}
 	}
 }
--- src/de/crysxd/ownfx/Profile.java
+++ src/de/crysxd/ownfx/Profile.java
@@ -1,7 +1,15 @@
 package de.crysxd.ownfx;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Vector;
+
+import com.google.gson.GsonBuilder;
 
 public class Profile {
 	
@@ -9,23 +17,71 @@ public class Profile {
 	private int id;
 	private List<Frame> frames = new Vector<>();
 	
+	public Profile() {
+		
+	}
+
 	public String getName() {
 		return name;
+		
 	}
 	
 	public void setName(String name) {
 		this.name = name;
+		
 	}
 	
 	public int getId() {
 		return id;
+		
 	}
 	
 	public void setId(int id) {
 		this.id = id;
+		
 	}
 	
 	public List<Frame> getFrames() {
 		return frames;
+		
+	}
+	
+	public static Profile readProfile(String json) {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(ColorStop.class, new ColorStop.JsonDeserializer());
+		
+		return gsonBuilder.create().fromJson(json, Profile.class);
+		
+	}
+	
+	public static Profile readProfile(InputStream jsonStream) throws IOException {
+		return Profile.readProfile(Profile.readStream(jsonStream));
+		
+	}
+	
+	
+	public static Profile readProfile(File jsonFile) throws IOException {
+		return Profile.readProfile(new FileInputStream(jsonFile));
+		
+	}
+	
+	private static String readStream(InputStream in) throws IOException {
+		BufferedReader inReader = new BufferedReader(new InputStreamReader(in));
+		StringBuilder readContent = new StringBuilder();
+		String line;
+		
+		try {
+			while((line = inReader.readLine()) != null) {
+				readContent.append(line);
+				readContent.append("\n");
+				
+			}	
+		} finally {
+			inReader.close();
+	
+		}
+		
+		return readContent.toString();
+		
 	}
 }
