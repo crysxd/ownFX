8d643daa7226d6fd939966181b937561aaaa92d9
crysxd <c.wuerthner@me.com>
2014-10-06 17:25:11 +0200

    Added ResourceProvider for webpage

---
+++ web/de/crysxd/ownfx/webpage/WebpageResourceProvider.java
@@ -0,0 +1,15 @@
+package de.crysxd.ownfx.webpage;
+import java.io.InputStream;
+
+import de.crysxd.ownfx.http.ResourceProvider;
+
+
+public class WebpageResourceProvider implements ResourceProvider {
+
+	@Override
+	public InputStream getResourceAsStream(String name) {
+		return this.getClass().getResourceAsStream(name);
+		
+	}
+
+}
