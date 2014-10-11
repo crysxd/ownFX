475c8710c6502256c49c96e7e98cd65a064a28e0
crysxd <c.wuerthner@me.com>
2014-10-11 11:13:43 +0200

    Added save and undo buttons

---
+++ web/js/settings.js
@@ -0,0 +1,18 @@
+$(function() {
+  settings.load(updateUi);
+  
+});
+
+var fieldRamSize = $('#ramSize');
+var fieldEepromSize = $('#eepromSize');
+var fieldLedCount = $('#ledCount');
+var fieldNeopixlesPin = $('#neopixlesPin');
+var fieldSerialInterface = $('#serialInterface');
+var fieldMaximumFrames = $('#maximumFrames');
+var fieldMaximumColorStops = $('#maximumColorStops');
+var progressbarRam = $('#rprogressRam');
+var progressbarEeprom = $('#progressEeprom');
+
+function updateUi() {
+  
+}
