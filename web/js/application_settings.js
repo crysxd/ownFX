f343032e283102f339be6519bd401ff001c3888a
crysxd <c.wuerthner@me.com>
2014-10-11 10:54:10 +0200

    Added application_settings.js to load and save settings

---
+++ web/js/application_settings.js
@@ -0,0 +1,44 @@
+var settings = new Object();
+
+settings.urlSettingsLoad = 'rest/settings';
+settings.urlSettingsSave = 'rest/settings_save';
+settings.loadedSettings = undefined;
+
+settings.load = function(callback) {
+  loadURLAsync(this.urlSettingsLoad, function(state, result) {
+    if(state == 200) {
+      try {
+        settings.loadedSettings = JSON.parse(result);
+        settings.maxColorStopsCount = settings.loadedSettings.maxColorStops;
+        settings.maxFrameCount = settings.loadedSettings.maxFrameCount;
+        settings.maxLedIndex = settings.loadedSettings.ledCount - 1;
+        
+        if(callback != undefined) {
+          callback();
+
+        }
+      } catch(e) {
+        alert("Error while loading settings from server!");
+
+      }
+    } else {
+      alert("Error while loading settings from server!");
+      
+    }
+  });
+}
+
+settings.save = function(callback) {
+  var postParams = "settings=" + JSON.parse(settings.loadedSettings);
+  loadURLAsync(this.urlSettingsSave, function(state, result) {
+     if(state == 200) {
+      if(callback != undefined) {
+        callback();
+        
+      }
+    } else {
+      alert("Error while saving settings from server!");
+      
+    }
+  }, postParams); 
+}
