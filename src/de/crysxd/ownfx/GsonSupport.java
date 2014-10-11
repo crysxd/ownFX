a23e996464ceec73e380879cfc90a18b3d720123
crysxd <c.wuerthner@me.com>
2014-10-11 16:25:44 +0200

    Added methods to stringify a object

--- src/de/crysxd/ownfx/GsonSupport.java
+++ src/de/crysxd/ownfx/GsonSupport.java
@@ -1,9 +1,12 @@
 package de.crysxd.ownfx;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -26,6 +29,18 @@ public class GsonSupport {
 	
 	public static <T> T parse(InputStream jsonStream, Class<T> outputClass) throws IOException {
 		return GsonSupport.<T>parse(GsonSupport.readStream(jsonStream), outputClass);
+		
+	}
+	
+	public static String stringify(Object o) {
+		return GsonSupport.createGson().toJson(o);
+		
+	}
+	
+	public static void stringify(Object o, OutputStream out) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+		bw.write(GsonSupport.stringify(o));
+		bw.close();
 		
 	}
 	
