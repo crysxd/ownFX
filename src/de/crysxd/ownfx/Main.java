a7f469482578ef31ff2035ec4193ff47d5498a6e
crysxd <c.wuerthner@me.com>
2014-12-23 17:52:39 +0100

    Now saving the selected com port to a file

--- src/de/crysxd/ownfx/Main.java
+++ src/de/crysxd/ownfx/Main.java
@@ -2,7 +2,13 @@ package de.crysxd.ownfx;
 
 import gnu.io.PortInUseException;
 import gnu.io.UnsupportedCommOperationException;
+
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
 import javax.naming.CommunicationException;
 import javax.swing.UIManager;
@@ -34,11 +40,33 @@ public class Main {
 	
 	private final ArduinoCommunicator ARDUINO_COM;
 	private String comPort;
+	private final File COM_PORT_FILE = new File(System.getProperty("user.home"), "\\AppData\\Local\\ownFX\\com");
 	
 	public Main() {
 		this.ARDUINO_COM = new ArduinoCommunicator();
 		Color currentColor = null;
-		this.comPort = Ui.queryComPort();
+		this.comPort = null;
+		
+		//Try to read the com port. If not successfull or if it no longer exists query the user for a new one
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(this.COM_PORT_FILE));
+			comPort = br.readLine();
+			br.close();
+			
+		} catch(Exception e) {
+			this.comPort = Ui.queryComPort();
+			
+			try {
+				this.COM_PORT_FILE.getParentFile().mkdirs();
+				BufferedWriter bw = new BufferedWriter(new FileWriter(this.COM_PORT_FILE));
+				bw.write(this.comPort);
+				bw.close();
+				
+			} catch(Exception e2) {
+				Ui.showErrorDialog("Unable to save the COM port. You will be asked again the next time.\nDetailes: " + e2);
+				
+			}
+		}
 		
 		try {
 			currentColor = this.ARDUINO_COM.start(this.comPort);
